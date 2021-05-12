64fa103f52564c87b34559559f32e4fc0b49b149
tungkoa <nguyentungkoa@gmail.com>
2021-05-12 09:13:42 +0700

    12/5/21 simple firebase

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +11,23 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private store: AngularFirestore,
+    private db: AngularFireDatabase
+  ) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.store
+      .collection('words')
+      .valueChanges()
+      .subscribe((res) => {
+        console.log(res);
+      });
+    this.db
+      .list('/words')
+      .valueChanges()
+      .subscribe((res) => console.log(res));
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBYJjTzFzHmH4VQGZW39I8gaR_D1AQgNco",
+    authDomain: "my-learning-english-id.firebaseapp.com",
+    databaseURL: "https://my-learning-english-id-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "my-learning-english-id",
+    storageBucket: "my-learning-english-id.appspot.com",
+    messagingSenderId: "328203299241",
+    appId: "1:328203299241:web:58f1662154abbfcbb53cb8",
+    measurementId: "G-GS9VJCP5TW"
+  }
 };
 
 /*
