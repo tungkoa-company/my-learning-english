4cac0ef3c6fe2bb4632a8ee60ac17b9b627af342
tungkoa <nguyentungkoa@gmail.com>
2021-05-15 17:58:13 +0700

    add: add angular-cli-ghpages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBYJjTzFzHmH4VQGZW39I8gaR_D1AQgNco',
+    authDomain: 'my-learning-english-id.firebaseapp.com',
+    databaseURL:
+      'https://my-learning-english-id-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'my-learning-english-id',
+    storageBucket: 'my-learning-english-id.appspot.com',
+    messagingSenderId: '328203299241',
+    appId: '1:328203299241:web:58f1662154abbfcbb53cb8',
+    measurementId: 'G-GS9VJCP5TW',
+  },
 };
